45646cae1ede179325451d588de4e49dcae5caa2
21Esteban <jelc200521@gmail.com>
2024-03-28 01:49:27 -0500

    Algoritmo de codificacion hecho

--- funciones.h
+++ funciones.h
@@ -11,6 +11,8 @@ void abrirArchivo(fstream &archivo, const char *filename);
 void cerrarArchivo(fstream &archivo);
 void obtenerCaracteres(fstream &archivo, const char *filename,char *array,int &longitud);
 void decimalBinario(char *arreglo, char arregloAGuardar[][9],int longitud);
+int determinarBitsAInvertir(char bloque[], int semilla);
+void codificacion(char arregloEnBinario[][9] ,int longitud,int semilla);
 
 #endif // FUNCIONES_H
 
